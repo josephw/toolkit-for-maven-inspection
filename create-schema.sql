a72c034f661a0a33384d0e08ad6bf178aebdf3ba
Joseph Walton <joe@kafsemo.org>
2022-01-03 23:38:39 +1100

    Pretty-print SQL, and add primary keys.

--- create-schema.sql
+++ create-schema.sql
@@ -1,7 +1,22 @@
 PRAGMA journal_mode=WAL;
 
-CREATE TABLE commits (commit_id VARCHAR NOT NULL PRIMARY KEY, stamp DATETIME NOT NULL, author_email VARCHAR NOT NULL);
+CREATE TABLE commits (
+  commit_id VARCHAR NOT NULL PRIMARY KEY,
+  stamp DATETIME NOT NULL,
+  author_email VARCHAR NOT NULL
+);
 
-CREATE TABLE commit_stats (commit_id VARCHAR NOT NULL, statistic VARCHAR NOT NULL, value NUMBER NOT NULL);
+CREATE TABLE commit_stats (
+  commit_id VARCHAR NOT NULL,
+  statistic VARCHAR NOT NULL,
+  value NUMBER NOT NULL,
+  PRIMARY KEY (commit_id, statistic)
+);
 
-CREATE TABLE module_stats (commit_id VARCHAR NOT NULL, module VARCHAR NOT NULL, statistic VARCHAR NOT NULL, value NUMBER NOT NULL);
+CREATE TABLE module_stats (
+  commit_id VARCHAR NOT NULL,
+  module VARCHAR NOT NULL,
+  statistic VARCHAR NOT NULL,
+  value NUMBER NOT NULL,
+  PRIMARY KEY (commit_id, module, statistic)
+);
