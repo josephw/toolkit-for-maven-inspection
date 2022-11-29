757d67fab02808f83f9aae57098ecbb11f3a9821
Joseph Walton <joe@kafsemo.org>
2022-11-29 19:46:33 +1100

    Fixes for the maven-project-structure example analysis.
    - Work with older sqlites
    - Add a missed script

--- examples/maven-project-structure/dependencies-to-dot.sh
+++ examples/maven-project-structure/dependencies-to-dot.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 { echo 'digraph {'
- sqlite3 record.db 'SELECT DISTINCT FORMAT("""%s"" -> ""%s"";", coords, dependency_coords) FROM maven_dependencies WHERE dependency_coords IN (SELECT coords FROM maven_dependencies);'
- sqlite3 record.db 'SELECT DISTINCT FORMAT("""%s"" [label=<%s<br />%,d lines>]", maven_modules.coords, maven_modules.coords, module_stats.value) FROM maven_modules JOIN module_stats ON maven_modules.commit_id = module_stats.commit_id AND maven_modules.path = module_stats.module WHERE module_stats.statistic="java-source-line-count";'
+ sqlite3 record.db 'SELECT DISTINCT PRINTF("""%s"" -> ""%s"";", coords, dependency_coords) FROM maven_dependencies WHERE dependency_coords IN (SELECT coords FROM maven_dependencies);'
+ sqlite3 record.db 'SELECT DISTINCT PRINTF("""%s"" [label=<%s<br />%,d lines>]", maven_modules.coords, maven_modules.coords, module_stats.value) FROM maven_modules JOIN module_stats ON maven_modules.commit_id = module_stats.commit_id AND maven_modules.path = module_stats.module WHERE module_stats.statistic="java-source-line-count";'
 
  echo '}'
 } >dependency-graph.dot
---
+++ index-pom.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+# Populate maven_modules by recursively parsing the submodules of a provided pom.xml
+
+from sys import argv
+from lxml import etree
+from sqlite3 import dbapi2
+
+import logging
+
+import argparse
+
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--database", required=True)
+parser.add_argument("--commit-id", required=True)
+parser.add_argument("pom_files", metavar="pom", nargs="+")
+
+args = parser.parse_args()
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
+log = logging.getLogger("index-pom")
+
+connection = dbapi2.connect(args.database)
+cursor = connection.cursor()
+
+namespaces = {"pom": "http://maven.apache.org/POM/4.0.0"}
+
+attempted = set()
+candidates = set(args.pom_files)
+
+while p := next(iter(candidates - attempted), None):
+    attempted.add(p)
+
+    log.info("Parsing %s", p)
+    doc = etree.parse(p).getroot()
+
+    projectList = doc.xpath("/pom:project", namespaces=namespaces)
+    if not projectList:
+        log.error("Skipping %s due to unexpected root element: %s", p, doc.tag)
+        continue
+
+    project = projectList[0]
+
+    groupIdList = project.xpath("pom:groupId/text()", namespaces=namespaces)
+    if not groupIdList:
+        groupIdList = project.xpath(
+            "pom:parent/pom:groupId/text()", namespaces=namespaces
+        )
+    if not groupIdList:
+        log.error("Skipping %s due to lack of groupId", p)
+        continue
+
+    groupId = groupIdList[0]
+
+    artifactId = project.xpath("pom:artifactId/text()", namespaces=namespaces)[0]
+
+    cursor.execute(
+        "INSERT INTO maven_modules (commit_id, coords, path) VALUES (?, ?, ?)",
+        [args.commit_id, groupId + ":" + artifactId, Path(p).parent.as_posix()],
+    )
+
+    # Find other modules
+    for m in project.xpath("pom:modules/pom:module/text()", namespaces=namespaces):
+        log.info("In %s found reference to module %s", p, m)
+        candidates.add((Path(p).parent / m / "pom.xml").as_posix())
+
+
+cursor.close()
+connection.commit()
+connection.close()
